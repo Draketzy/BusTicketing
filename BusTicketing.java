2bceb38a300f7a11067e12a459e1d14191927e98
Karlzkie07 <133963244+Karlzkie07@users.noreply.github.com>
2023-05-18 18:26:12 +0800

    gwapo

--- BusTicketing.java
+++ BusTicketing.java
@@ -3,6 +3,6 @@ public class BusTicketing
     public static void main(String[] args) {
         System.out.print("Hi...");
         System.out.print("Hellooooooo!!");
-        
+        System.out.print("Gwapo ko");
     }
 }
